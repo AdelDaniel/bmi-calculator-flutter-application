4993dddaef00aa7119122726c6351ec087b86462
adel_daniel <adel.daniel.222@gmail.com>
2020-11-22 18:19:51 +0200

    hide keys

--- lib/services/recipe_service.dart
+++ lib/services/recipe_service.dart
@@ -1,19 +1,17 @@
-
 import 'dart:convert';
 import '../model/recipe.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class RecipeService extends GetxController {
-
-  List<MainItem> mainItems =[];
+  List<MainItem> mainItems = [];
   //List hits = [];
   static RxList _recipes = List<Recipe>().obs;
   static const String _applicationID = "acfdcdf7";
-  static const  String _applicationKey = '2c2a9bbb08a38991897cb7bdc5c3b493';
+  static const String _applicationKey = '2c2a9bbb08a38991897cb7bdc5c3b493';
 
-
-  Future getAllRecipes(String searchWord) async{
+  Future getAllRecipes(String searchWord) async {
+    _recipes.clear();
     String url =
         "https://api.edamam.com/search?q=$searchWord&app_id=$_applicationID&app_key=$_applicationKey";
     var response = await http.get(url);
@@ -21,14 +19,13 @@ class RecipeService extends GetxController {
     Map<String, dynamic> jsonDataBody = jsonDecode(response.body);
     //  print("this is json Data $jsonDataBody"); // good
     jsonDataBody["hits"].forEach((element) {
-    //   print(element.toString());
+      //   print(element.toString());
       Recipe recipe = Recipe.fromJson(element['recipe']);
-    //  print(recipe.uri);
-      _recipes.insert(0,recipe);
+      //  print(recipe.uri);
+      _recipes.add(recipe);
     });
     return;
   }
 
   static get getRecipes => _recipes;
-
 }
